766e11c3c57e24002ddf60d6234f0e65a6df61c1
Justin Lai <justin.th.lai@gmail.com>
2016-05-24 17:18:03 -0700

    implemented change current position in song feature for playback

--- public/app/player/player.js
+++ public/app/player/player.js
@@ -2,11 +2,18 @@ angular.module('MIDIPlayer.player', [])
 
 .controller('PlayerController', function($scope, PlayerControls){
   var player = MIDI.Player;
-  $scope.files = ['summer.mid', 'twinkle_twinkle.mid'];
+  $scope.files = ['twinkle_twinkle.mid', 'summer.mid', 'bumble_bee.mid'];
 
   $scope.play = PlayerControls.play;
   $scope.pause = player.pause;
   $scope.stop = player.stop;
+  $scope.setTime = function(event) {
+    var percent = event.offsetX / $('#player-track-full').width();
+    $scope.stop();
+    MIDI.Player.currentTime = percent * MIDI.Player.endTime;
+    $scope.play();
+  }
 
-  $scope.trackWidth = 0;
+
+
 });
